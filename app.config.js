0ec9a9baa319bbe92a5a2bdbe704abeea4134fa3
pathwei <officialpathayapp@gmail.com>
2025-07-07 17:35:26 +0200

    final version: no notifications

--- app.config.js
+++ app.config.js
@@ -4,8 +4,10 @@ export default {
     slug: "cafftracker",
     version: "1.0.0",
     orientation: "portrait",
+    icon: "./assets/images/icon.png",
     userInterfaceStyle: "dark",
-    scheme: "cafftracker", // ADD THIS for return URL
+    scheme: "cafftracker",
+    newArchEnabled: true,
     splash: {
       resizeMode: "contain",
       backgroundColor: "#000000",
@@ -17,9 +19,36 @@ export default {
     },
     android: {
       adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#000000",
       },
       package: "com.yourcompany.cafftracker",
+      edgeToEdgeEnabled: true,
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png",
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#000000",
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+    },
+    extra: {
+      eas: {
+        projectId: "a0afabda-ea43-4bb0-8afe-97e03509105f",
+      },
     },
   },
 };
